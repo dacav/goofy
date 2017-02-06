395a4c6823a8e4cfdafcd235bd40b380620b3023
Giovanni Simoni <dacav@openmailbox.org>
2017-02-06 17:20:45 +0100

    semi-decent main accepts

--- src/main.cpp
+++ src/main.cpp
@@ -5,71 +5,109 @@
 #include "gopher/node-term-write.h"
 #include "gopher/map.h"
 
+#include "parse/ip.h"
+
 #include <iostream>
 #include <map>
 #include <memory>
 #include <sstream>
 
 #include <event2/event.h>
+#include <event2/listener.h>
 
 #include <unistd.h> // tmp, use for write
 #include <ext/stdio_filebuf.h>
 
-const uint16_t PORT = 70;
+struct GlobalContext
+{
+    spg::gopher::Map map;
+};
 
 namespace
 {
-    spg::gopher::Map map;
 
-    void cb_accept(evutil_socket_t fd, short what, void *arg)
+    void cb_accept(
+            struct evconnlistener* listener,
+            int clsocket,
+            struct sockaddr *claddress,
+            int claddrlen,
+            void *context)
     {
-        spg::Server &srv = *reinterpret_cast<spg::Server *>(arg);
-        spg::Client cl = srv.accept();
-
-        char buffer[1024];
-        int n = read(cl.fd, buffer, sizeof buffer);
-        std::cerr << "Received query: [" << buffer << "]" << std::endl;
-        map.lookup(std::string(buffer, n - 2)).show(cl.fd);
+        std::cerr << "Connected yo " << clsocket << std::endl;
     }
 
-    void main_loop (spg::Server &srv, event_base *base)
+    void cb_accept_err(
+            struct evconnlistener *listener,
+            void *context)
     {
-        struct event *ev_accept = event_new(base, srv.fd,
-            EV_READ | EV_PERSIST,
-            cb_accept,
-            reinterpret_cast<void *>(&srv)
-        );
-
-        event_add(ev_accept, NULL);
-
-        switch (event_base_dispatch(base)) {
-            case -1: // error
-                std::cerr << "error" << std::endl;
-                break;
-            case 0: // exited normally
-                std::cerr << "exited normlly" << std::endl;
-                break;
-            case 1: // no more events
-                std::cerr << "no more events" << std::endl;
-                break;
-        }
+        struct event_base *base = evconnlistener_get_base(listener);
+        std::cerr << "Error while accepting: "
+            << strerror(errno) << std::endl;
+        event_base_loopexit(base, NULL);
     }
-}
+
+} // anon namespace
 
 int main(int argc, char **argv)
 {
-    spg::Server srv = spg::Server::parse_addr(argv[1], PORT, 8);
+    // Stuff to put in configuration
+    const char* ip = "::1";
+    const uint16_t port = 8070;
+    const unsigned backlog = 10;
+    const bool sock_reusable = true;
+
+    if (argc > 1) {
+        ip = argv[0];
+    }
+
+    GlobalContext context;
     {
         using namespace spg::gopher;
-        auto& root = map.mknode<NodeDirList>("root", "", "::1", PORT);
-        auto& l1 = map.mknode<NodeDirList>("le boobs", "le/boobs", "::1", PORT);
-        auto& l2 = map.mknode<NodeDirList>("le boobies", "le/boobies", "::1", PORT);
+        auto& root = context.map.mknode<NodeDirList>("root", "", ip, port);
+        auto& l1 = context.map.mknode<NodeDirList>("le boobs", "le/boobs", ip, port);
+        auto& l2 = context.map.mknode<NodeDirList>("le boobies", "le/boobies", ip, port);
         root.insert(l1);
         l1.insert(l2);
         l2.insert(l1);
     }
 
-    event_base *base = event_base_new();
-    main_loop(srv, base);
-    event_base_free(base);
+    struct sockaddr_storage addr_storage;
+    std::unique_ptr<struct event_base, void(*)(struct event_base*)> base(
+        event_base_new(),
+        event_base_free
+    );
+    std::unique_ptr<struct evconnlistener, void(*)(struct evconnlistener*)> listener(
+        evconnlistener_new_bind(
+            base.get(),
+            cb_accept,
+            &context,
+            LEV_OPT_CLOSE_ON_FREE | unsigned(sock_reusable) * LEV_OPT_REUSEABLE,
+            backlog,
+            spg::parse::ipaddr(addr_storage, ip, port),
+            sizeof(addr_storage)
+        ),
+        evconnlistener_free
+    );
+    evconnlistener_set_error_cb(listener.get(), cb_accept_err);
+
+    if (listener.get() == nullptr) {
+        std::cerr << "Cannot listen: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // todo: stderr of libevent
+    switch (event_base_dispatch(base.get())) {
+        case -1: // error
+            std::cerr << "error from libevent" << std::endl;
+            exit(EXIT_FAILURE);
+            break;
+        case 0: // exited normally
+            std::cerr << "terminated normally" << std::endl;
+            exit(EXIT_SUCCESS);
+            break;
+        case 1: // no more events
+            std::cerr << "no more events" << std::endl;
+            exit(EXIT_SUCCESS);
+            break;
+    }
 }
