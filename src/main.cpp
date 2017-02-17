f00776e38c05623b3ce13a2a809543b9ae7fcebc
Giovanni Simoni <dacav@openmailbox.org>
2017-02-17 14:45:18 +0100

    Crappy main factorization
    Good enough for now.

--- src/main.cpp
+++ src/main.cpp
@@ -23,59 +23,115 @@
 
 namespace
 {
-    struct GlobalContext
+    const char* ip = "::1";
+    const uint16_t port = 7070;
+    const unsigned backlog = 10;
+    const bool sock_reusable = true;
+
+    struct Server
     {
+        Server();
         spg::gopher::Map gopher_map;
 
-        using EventBase = std::unique_ptr<
+        std::unique_ptr<
             struct event_base,
             void(*)(struct event_base*)
-        >;
-        EventBase base_event;
+        > base_event;
 
-        using SessionMap = std::unordered_map<
+        std::unique_ptr<
+            struct evconnlistener,
+            void(*)(struct evconnlistener*)
+        > tcp_listener;
+
+        std::unique_ptr<
+            struct event,
+            void(*)(struct event*)
+        > sighandler;
+
+        std::unordered_map<
             unsigned,
             std::unique_ptr<spg::session::Session>
-        >;
-        SessionMap sessions;
+        > sessions;
 
-        GlobalContext()
-            : base_event(event_base_new(), event_base_free)
-        {}
+        void drop_session(unsigned session_id);
+        spg::session::Session& new_session(int clsock);
+        void start();
 
-        void drop_session(unsigned session_id)
-        {
-            sessions.erase(session_id);
-        }
-
-        spg::session::Session& new_session(int clsock)
-        {
-            using namespace std::placeholders;
-
-            const unsigned next_id = sessions.size();
-            std::unique_ptr<spg::session::Session> session(
-                new spg::session::Session(
-                    gopher_map,
-                    std::bind(&GlobalContext::drop_session, this, next_id),
-                    clsock,
-                    base_event.get()
-                )
-            );
-            auto out = sessions.emplace(next_id, std::move(session));
-            assert(out.second);
-            return *session.get();
-        }
+        static void cb_accept(
+                struct evconnlistener* tcp_listener,
+                int clsocket,
+                struct sockaddr *claddress,
+                int claddrlen,
+                void *context);
+        static void cb_accept_err(
+                struct evconnlistener *tcp_listener,
+                void *context);
+        static void cb_signal(
+                int sig,
+                short what,
+                void *ctx);
     };
 
-    void cb_accept(
-            struct evconnlistener* listener,
+    Server::Server() :
+        base_event(event_base_new(), event_base_free),
+        tcp_listener(nullptr, evconnlistener_free),
+        sighandler(nullptr, event_free)
+    {
+        struct sockaddr_storage addr_storage;
+
+        tcp_listener.reset(evconnlistener_new_bind(
+            base_event.get(),
+            Server::cb_accept,
+            this,
+            LEV_OPT_CLOSE_ON_FREE | unsigned(sock_reusable) * LEV_OPT_REUSEABLE,
+            backlog,
+            spg::parse::ipaddr(addr_storage, ip, port),
+            sizeof(addr_storage)
+        ));
+        if (tcp_listener.get() == nullptr) {
+            std::cerr << "Cannot listen: " << strerror(errno) << std::endl;
+            throw 1;
+        }
+        evconnlistener_set_error_cb(tcp_listener.get(), cb_accept_err);
+
+        sighandler.reset(evsignal_new(
+            base_event.get(), SIGINT, cb_signal, this
+        ));
+        event_add(sighandler.get(), NULL);
+    }
+
+    void Server::drop_session(unsigned session_id)
+    {
+        sessions.erase(session_id);
+    }
+
+    spg::session::Session& Server::new_session(int clsock)
+    {
+        using namespace std::placeholders;
+
+        const unsigned next_id = sessions.size();
+        std::unique_ptr<spg::session::Session> session(
+            new spg::session::Session(
+                gopher_map,
+                std::bind(&Server::drop_session, this, next_id),
+                clsock,
+                base_event.get()
+            )
+        );
+        auto out = sessions.emplace(next_id, std::move(session));
+        assert(out.second);
+        return *session.get();
+    }
+
+    void Server::cb_accept(
+            struct evconnlistener* tcp_listener,
             int clsocket,
             struct sockaddr *claddress,
             int claddrlen,
             void *context)
     {
         try {
-            reinterpret_cast<GlobalContext*>(context)->new_session(clsocket);
+            reinterpret_cast<Server*>(context)->new_session(clsocket);
         }
         catch (spg::session::SessionError& error) {
             std::cerr << "Creating session: "
@@ -83,23 +139,43 @@ namespace
         }
     }
 
-    void cb_accept_err(
-            struct evconnlistener *listener,
+    void Server::cb_accept_err(
+            struct evconnlistener *tcp_listener,
             void *context)
     {
-        struct event_base *base = evconnlistener_get_base(listener);
+        struct event_base *base = evconnlistener_get_base(tcp_listener);
         std::cerr << "Error while accepting: "
             << strerror(errno) << std::endl;
         event_base_loopexit(base, NULL);
     }
 
-    void cb_signal(int sig, short what, void *ctx)
+    void Server::cb_signal(
+            int sig,
+            short what,
+            void *ctx)
     {
-        GlobalContext& globals = *reinterpret_cast<GlobalContext*>(ctx);
+        Server& srv = *reinterpret_cast<Server*>(ctx);
 
         assert(what & EV_SIGNAL);
         std::cerr << "Got signal " << sig << std::endl;
-        event_base_loopbreak(globals.base_event.get());
+        event_base_loopbreak(srv.base_event.get());
+    }
+
+    void Server::start()
+    {
+        // todo: take stderr of libevent for logging.
+        switch (event_base_dispatch(base_event.get()))
+        {
+            case -1: // error
+                std::cerr << "error from libevent" << std::endl;
+                throw 1;
+            case 0: // exited normally
+                std::cerr << "terminated normally" << std::endl;
+                throw 0;
+            case 1: // no more events
+                std::cerr << "no more events" << std::endl;
+                throw 0;
+        }
     }
 
 } // anon namespace
@@ -107,65 +183,25 @@ namespace
 int main(int argc, char **argv)
 {
     // Stuff to put in configuration
-    const char* ip = "::1";
-    const uint16_t port = 7070;
-    const unsigned backlog = 10;
-    const bool sock_reusable = true;
-
     if (argc > 1) {
         ip = argv[0];
     }
 
-    GlobalContext globals;
+    Server srv;
     {
         using namespace spg::gopher;
-        auto& root = globals.gopher_map.mknode<NodeMenu>("root", "", "localhost", port);
-        auto& l1 = globals.gopher_map.mknode<NodeMenu>("le boobs", "le/boobs", "localhost", port);
-        auto& l2 = globals.gopher_map.mknode<NodeMenu>("le boobies", "le/boobies", "localhost", port);
+        auto& root = srv.gopher_map.mknode<NodeMenu>("root", "", "localhost", port);
+        auto& l1 = srv.gopher_map.mknode<NodeMenu>("le boobs", "le/boobs", "localhost", port);
+        auto& l2 = srv.gopher_map.mknode<NodeMenu>("le boobies", "le/boobies", "localhost", port);
         root.insert(l1);
         l1.insert(l2);
         l2.insert(l1);
     }
 
-    struct sockaddr_storage addr_storage;
-    std::unique_ptr<struct evconnlistener, void(*)(struct evconnlistener*)> listener(
-        evconnlistener_new_bind(
-            globals.base_event.get(),
-            cb_accept,
-            &globals,
-            LEV_OPT_CLOSE_ON_FREE | unsigned(sock_reusable) * LEV_OPT_REUSEABLE,
-            backlog,
-            spg::parse::ipaddr(addr_storage, ip, port),
-            sizeof(addr_storage)
-        ),
-        evconnlistener_free
-    );
-    evconnlistener_set_error_cb(listener.get(), cb_accept_err);
-
-    std::unique_ptr<struct event, void(*)(struct event*)> sighandler(
-        evsignal_new(globals.base_event.get(), SIGINT, cb_signal, &globals),
-        event_free
-    );
-    event_add(sighandler.get(), NULL);
-
-    if (listener.get() == nullptr) {
-        std::cerr << "Cannot listen: " << strerror(errno) << std::endl;
-        return 1;
+    try {
+        srv.start();
     }
-
-    // todo: stderr of libevent
-    switch (event_base_dispatch(globals.base_event.get())) {
-        case -1: // error
-            std::cerr << "error from libevent" << std::endl;
-            return 1;
-            break;
-        case 0: // exited normally
-            std::cerr << "terminated normally" << std::endl;
-            return 0;
-            break;
-        case 1: // no more events
-            std::cerr << "no more events" << std::endl;
-            return 0;
-            break;
+    catch (int ret) {
+        return ret;
     }
 }
